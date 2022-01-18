b112525653a967e270b897272b4d16306fab7b78
Justin Wu <justinwutang@gmail.com>
2022-01-17 20:56:11 -0500

    Create main and helper function

---
+++ main.py
@@ -0,0 +1,26 @@
+#=====================================================================
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#---------------------------------------------------------------------
+# Class:         CAP6908 - Independent Studies
+# Professor:     Professor Haiyan Hu
+# Name:          Justin Wu
+# Project:       Skin Disease Classifier
+# Function:      main.py
+# Create:        01/17/22
+# Description:   Main function to call other functions
+#---------------------------------------------------------------------
+
+# Import
+from process_Data import *
+
+# Funtion
+def main():
+    print("Hello World!")
+    proccess_Data()
+
+# Modules
+if __name__ == "__main__":
+    main()
+
+#=====================================================================
---
+++ process_Data.py
@@ -0,0 +1,20 @@
+#=====================================================================
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#---------------------------------------------------------------------
+# Class:         CAP6908 - Independent Studies
+# Professor:     Professor Haiyan Hu
+# Name:          Justin Wu
+# Project:       Skin Disease Classifier
+# Function:      process_Data.py
+# Create:        01/17/22
+# Description:   Function used to gather HAM1000 dataset and process data
+#---------------------------------------------------------------------
+
+# Import
+
+# Function
+def proccess_Data():
+    print("Hi There")
+
+#=====================================================================
